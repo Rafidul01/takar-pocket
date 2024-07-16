aa84e6864ceea69ba65722ad9e89a28c6998aa9a
Rafidul01 <rafidulhasan555@gmail.com>
2024-07-16 15:54:52 +0600

    react tailweed daisy setup complited

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+
+const MainLayout = () => {
+    return (
+        <div>
+
+            <h1 className="text-3xl font-bold text-red-500">Hello</h1>
+            
+        </div>
+    );
+};
+
+export default MainLayout;
