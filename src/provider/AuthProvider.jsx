5d19a7e2bc74657c1d714d7dc45263a7a6b8b8b0
Rafidul01 <rafidulhasan555@gmail.com>
2024-07-18 01:24:30 +0600

    user saved in local storage

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import useAxiosPublic from "../hooks/useAxiosPublic";
 import Swal from "sweetalert2";
@@ -6,13 +6,25 @@ import Swal from "sweetalert2";
 export const AuthContext = createContext(null)
 const AuthProvider = ({ children }) => {
 
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem("userInfo")) || null);
+    
+
     const axiosPublic = useAxiosPublic();
 
-    const userRegister = (user) => {
+    const userRegister = (userData) => {
 
-        axiosPublic.post("/users", user)
+        const storeData = {
+            name: userData.name,
+            email: userData.email,
+            number: userData.number,
+            role: '',
+            
+        }
+
+        axiosPublic.post("/users", userData)
         .then((res) => {
             console.log(res);
+            setUser(storeData);
             Swal.fire({
                 position: "center",
                 icon: "success",
@@ -31,6 +43,17 @@ const AuthProvider = ({ children }) => {
               }); 
         })
     }
+
+    console.log(user);
+    useEffect(() => {
+        if(user) {
+            localStorage.setItem("userInfo", JSON.stringify(user));
+            return;
+        }
+
+        localStorage.removeItem("userInfo");  
+        
+      }, [user]);
 
 
     const authInfo = {
